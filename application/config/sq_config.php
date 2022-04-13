01b75dc1415f75af67b5b66c7830e2208f4104b6
nollyenterprise <47524528+nollyenterprise@users.noreply.github.com>
2022-04-13 20:29:17 +0100

    Update sq_config.php

--- application/config/sq_config.php
+++ application/config/sq_config.php
@@ -2,6 +2,6 @@
 		$sq_base_url='http://localhost/offline_portal';
 		$sq_hostname='localhost';
 		$sq_dbname='offline_portal';
-		$sq_dbusername='root';
+		$sq_dbusername='';
 		$sq_dbpassword='';
 ?>
